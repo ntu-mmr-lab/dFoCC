9581bd3b2884421f5c3c10c773a423a8c2a767bc
ntu-mmr-lab <ntummrlab468@gmail.com>
2025-12-18 16:15:09 +0800

    Regenerate the sample_results/ folder

--- tutorial/sample_results/E384_cycle001/coot.scm
+++ tutorial/sample_results/E384_cycle001/coot.scm
@@ -3,12 +3,12 @@
 (set-nomenclature-errors-on-read "ignore")
 
 ;; load the dark and the calculated light structure
-(read-pdb "./input/8Z1J.pdb")
-(read-pdb "E384_cycle001/rc_1200_600_0.pdb")
+(read-pdb "./input/8Z1J-without-H.pdb")
+(read-pdb "E384_cycle001/rc_1200_600_1800.pdb")
 
 ;; read the dFo region-extracted sigma-cutoff map
 ;; and show almost all signal on the map
-;; (handle-read-ccp4-map "E384_cycle001/dFo.rc_1200_600_0.masked_sigma3.0.ccp4" 1)
+;; (handle-read-ccp4-map "E384_cycle001/dFo.rc_1200_600_1800.masked_sigma3.0.ccp4" 1)
 ;; (set-last-map-contour-level-by-sigma 0.01)
 
 ;; read the entire dFo struct factor
@@ -20,13 +20,13 @@
 
 ;; read the dFc region-extracted sigma-cutoff map
 ;; show almost all signal on the map, and change its colour into cyan-yellow pair
-;; (handle-read-ccp4-map "E384_cycle001/dFc.rc_1200_600_0.masked_sigma3.0.ccp4" 1)
+;; (handle-read-ccp4-map "E384_cycle001/dFc.rc_1200_600_1800.masked_sigma3.0.ccp4" 1)
 ;; (set-last-map-contour-level-by-sigma 0.01)
 
 ;; read the entire dFo struct factor
 ;; apply suitable sigma cutoff on it
 ;; and change its colour into cyan-yellow pair
-(make-and-draw-map "E384_cycle001/dFc.rc_1200_600_0.mtz" "dFc" "dPHIc" "" 0 1)
-(set-last-map-contour-level-by-sigma 36.00)
+(make-and-draw-map "E384_cycle001/dFc.rc_1200_600_1800.mtz" "dFc" "dPHIc" "" 0 1)
+(set-last-map-contour-level-by-sigma 33.00)
 (set-last-map-colour 0.0 1.0 1.0)
 ; (export-map-fragment 3 (rotation-centre-position 0) (rotation-centre-position 1) (rotation-centre-position 2) 20.0 "E384_cycle001/dFc_for_pymol.map")
--- tutorial/sample_results/E384_cycle019/coot.scm
+++ tutorial/sample_results/E384_cycle019/coot.scm
@@ -3,7 +3,7 @@
 (set-nomenclature-errors-on-read "ignore")
 
 ;; load the dark and the calculated light structure
-(read-pdb "./input/8Z1J.pdb")
+(read-pdb "./input/8Z1J-without-H.pdb")
 (read-pdb "E384_cycle019/rc_0_0_0.pdb")
 
 ;; read the dFo region-extracted sigma-cutoff map
@@ -27,6 +27,6 @@
 ;; apply suitable sigma cutoff on it
 ;; and change its colour into cyan-yellow pair
 (make-and-draw-map "E384_cycle019/dFc.rc_0_0_0.mtz" "dFc" "dPHIc" "" 0 1)
-(set-last-map-contour-level-by-sigma 36.00)
+(set-last-map-contour-level-by-sigma 33.00)
 (set-last-map-colour 0.0 1.0 1.0)
 ; (export-map-fragment 3 (rotation-centre-position 0) (rotation-centre-position 1) (rotation-centre-position 2) 20.0 "E384_cycle019/dFc_for_pymol.map")
